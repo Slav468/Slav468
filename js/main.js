35ce077e9ccb2b46a33ab0d1151ff3384af0bb9b
Slav468 <slav1988@tut.by>
2023-02-07 15:50:32 +0300

    feat: update portfolio with 3 new works

--- js/main.js
+++ js/main.js
@@ -1,34 +1,38 @@
 //Burger menu//
 
-window.addEventListener('DOMContentLoaded', () => {
-    const hamburger = document.querySelector('.hamburger'),
-        menu = document.querySelector('.menu-deactive'),
-        closeElem = document.querySelector('.menu__close'),
-        wrapperMenu = document.querySelector('wrap');
+window.addEventListener("DOMContentLoaded", () => {
+	const hamburger = document.querySelector(".hamburger"),
+		menu = document.querySelector(".menu-deactive"),
+		closeElem = document.querySelector(".menu__close"),
+		wrapperMenu = document.querySelector("wrap"),
+		menuOverlay = document.querySelector(".menu__overlay");
 
-    hamburger.addEventListener('click', () => {
-        hamburger.classList.toggle('hamburger-deactive');
-        hamburger.classList.remove('hamburger');
-        menu.classList.toggle('menu');
-        menu.classList.remove('menu-deactive');
+	hamburger.addEventListener("click", () => {
+		hamburger.classList.toggle("hamburger-deactive");
+		hamburger.classList.remove("hamburger");
+		menu.classList.toggle("menu");
+		menu.classList.remove("menu-deactive");
+	});
 
-    });
+	closeElem.addEventListener("click", () => {
+		menu.classList.toggle("menu-deactive");
+		menu.classList.remove("menu");
+		hamburger.classList.toggle("hamburger");
+		hamburger.classList.remove("hamburger-deactive");
+	});
 
-    closeElem.addEventListener('click', () => {
-        menu.classList.toggle('menu-deactive');
-        menu.classList.remove('menu');
-        hamburger.classList.toggle('hamburger');
-        hamburger.classList.remove('hamburger-deactive');
+	menuOverlay.addEventListener("click", (e) => {
+		menu.classList.toggle("menu-deactive");
+		menu.classList.remove("menu");
+		hamburger.classList.toggle("hamburger");
+		hamburger.classList.remove("hamburger-deactive");
+	});
 
-    });
+	// skills prc //
+	const counters = document.querySelectorAll(".framework-skills__prc"),
+		lines = document.querySelectorAll(".framework-skills__ratings-line span");
 
-});
-
-
-// skills prc //
-const counters = document.querySelectorAll('.framework-skills__prc'),
-    lines = document.querySelectorAll('.framework-skills__ratings-line span');
-
-counters.forEach((item, i) => {
-    lines[i].style.width = item.innerHTML;
+	counters.forEach((item, i) => {
+		lines[i].style.width = item.innerHTML;
+	});
 });
